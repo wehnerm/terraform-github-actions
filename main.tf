edae57949bcfecb6ce23dfdfa527df466da9c648
Martin W <75484362+wehnerm@users.noreply.github.com>
2023-04-02 23:02:35 +0200

    update terraform configuration for pipeline

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      version = ">= 3.50.0"
     }
   }
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
     resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
+    storage_account_name = "tfghactionsstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
